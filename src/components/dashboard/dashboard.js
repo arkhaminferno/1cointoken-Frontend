76f21e03d42993fd610585f5b93320bdfe974f66
arkhaminferno <gauravpsp@gmail.com>
2020-08-03 16:21:15 +0530

    Fixed Panel

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -3,6 +3,7 @@ import { Row, Col, Container, Button } from "reactstrap";
 import { Nav, NavItem, NavLink, Input } from "reactstrap";
 import { FaTelegram, FaEye } from "react-icons/fa";
 import { FiGlobe } from "react-icons/fi";
+import { GrGroup } from "react-icons/gr";
 import "./dashboard.css";
 export default class dashboard extends Component {
   render() {
@@ -79,87 +80,107 @@ export default class dashboard extends Component {
             </div>
           </div>
         </Row>
-        <br />
+
         <Row>
-          <Col lg="3" mb="4" xs="3" className="wf1">
-            <div className="panel1">
-              <div className="sub-panel1">
-                <Row>
-                  <Col>lgog</Col>
-                  <Col>
-                    <Row>
-                      <Col>id123</Col>
-                    </Row>
-                    <Row>
-                      <Col>1234</Col>
-                      <Col>logo</Col>
-                    </Row>
-                    <Row>
-                      <Col>$ 0</Col>
-                    </Row>
-                    <Row></Row>
-                  </Col>
-                </Row>
-              </div>
+          <Col lg="4" mb="4" xs="3" className="wf1">
+            <Row>
+              <div className="panel1">
+                <div className="sub-panel1">
+                  <Row className="sub-panel1-padding">
+                    <Col className="sub-panel-1-logo-width">
+                      <img
+                        width="100%"
+                        height="100%"
+                        src="/assets/eth-logo.png"
+                      />
+                    </Col>
+                    <Col className="sub-panel-1-width">
+                      <Row>
+                        <Col className="id-">ID 123</Col>
+                      </Row>
+                      <Row>
+                        <div className="sub-panel-1-flex">
+                          <div className="sub-panel-1-flex-child"></div>
+                          <div className="sub-panel-1-flex-child2">
+                            0
+                            <img
+                              width="40%"
+                              height="40%"
+                              src="/assets/partners_light.svg"
+                            />
+                          </div>
+                        </div>
+                      </Row>
+                      <Row>
+                        <Col className="bal-dollars">$ 0</Col>
+                      </Row>
+                      <Row></Row>
+                    </Col>
+                  </Row>
+                </div>
 
-              <div className="sub-panel2">
-                <Row>
-                  <Col>
-                    <div className="balance-display">
-                      <Button>safasf</Button>
-                    </div>
-                  </Col>
-                </Row>
-              </div>
-
-              <div className="sub-panel3">
-                <Col>
-                  <Row>forsage x3</Row>
-                  <Row>0$</Row>
-                </Col>
-              </div>
-              <div className="sub-child-panel3">
-                <Row>
-                  <Col></Col>
-                  <Col>
-                    <Button>123</Button>
-                  </Col>
-                </Row>
-              </div>
-
-              <div className="sub-panel4">
-                <Col>
-                  <Row>forsage x4</Row>
-                  <Row>0$</Row>
-                </Col>
-              </div>
-              <div className="sub-child-panel4">
-                <Row>
-                  <Col></Col>
-                  <Col>
-                    <Button>123</Button>
-                  </Col>
-                </Row>
-              </div>
-              <Row>
-                <Col>
+                <div className="sub-panel2">
                   <Row>
                     <Col>
-                      <FaTelegram />
+                      <div className="balance-display">
+                        <Button id="balance-child-display">0.00eth</Button>
+                      </div>
                     </Col>
-                    <Col>Enable </Col>
                   </Row>
-                </Col>
-              </Row>
-              <Row>
-                <Col>
-                  <FiGlobe />
-                </Col>
-                <Col>USD</Col>
-                <Col>EUR</Col>
-                <Col>RUB</Col>
-              </Row>
-            </div>
+                </div>
+
+                <div className="sub-panel3">
+                  <div className="sb-child-panel">
+                    <Row>
+                      <Col>
+                        <div className="sub-panel3-child">
+                          <img height="100%" width="80%" src="assets/x3.svg" />
+                        </div>
+                        <div className="sub-panel3-child2">0$ &nbsp;</div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <div className="sub-child-panel3">
+                        <Button id="btn-eth">0.00 eth</Button>
+                      </div>
+                    </Row>
+                  </div>
+
+                  <div className="sb-child-panel">
+                    <Row>
+                      <Col>
+                        <div className="sub-panel3-child">
+                          <img height="100%" width="80%" src="assets/x4.svg" />
+                        </div>
+                        <div className="sub-panel3-child2">0$ &nbsp;</div>
+                      </Col>
+                    </Row>
+                    <Row>
+                      <div className="sub-child-panel3">
+                        <Button id="btn-eth">0.00 eth</Button>
+                      </div>
+                    </Row>
+                  </div>
+                </div>
+
+                <div className="notification-flex-row">
+                  <div className="telegram">
+                    <FaTelegram color="white" />
+                  </div>
+                  <div className="nfication"> Enable notifications</div>
+                </div>
+                <div className="currency">
+                  <div>
+                    <Row className="globe">
+                      <FiGlobe color="white" />
+                    </Row>
+                  </div>
+                  <div className="USD">USD</div>
+                  <div className="EUR"> EUR</div>
+                  <div className="RUB">RUB</div>
+                </div>
+              </div>
+            </Row>
             <br />
             <div className="panel2">
               <Col>
